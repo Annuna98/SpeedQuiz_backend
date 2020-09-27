d3d927c35d6e3fa6898a00bee15401070d3acee7
Myamikova Anna <myamikova9@gmail.com>
2020-09-27 18:38:01 +0600

    Initial commit

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a5e69dcd828330f1f42ea0afa172e96d'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '11b02e36-fc4f-410d-962f-7f3d02754e95'
-};
